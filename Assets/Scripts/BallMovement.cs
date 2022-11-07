b9bbb2930daa579ca26e582912a8ce3f108f18f3
Inheo <agch777505@gmail.com>
2022-11-07 09:08:22 +0300

    up

--- Assets/Scripts/BallMovement.cs
+++ Assets/Scripts/BallMovement.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using GG.Infrastructure.Utils.Swipe;
 using DG.Tweening;
 using System.Collections.Generic;
 using UnityEngine.Events;
@@ -7,12 +6,13 @@ using System.Linq;
 
 public class BallMovement : MonoBehaviour
 {
-    [SerializeField] private SwipeListener swipeListener;
-    [SerializeField] private LevelManager levelManager;
-
-    [SerializeField] private float stepDuration = 0.1f;
-    [SerializeField] private LayerMask wallsAndRoadsLayer;
     private const float MAX_RAY_DISTANCE = 100f;
+
+    [SerializeField] private InputService _inputService;
+    [SerializeField] private LevelManager _levelManager;
+
+    [SerializeField] private float _stepDuration = 0.1f;
+    [SerializeField] private LayerMask _wallsAndRoadsLayer;
 
     public UnityAction<List<RoadTile>, float> onMoveStart;
 
@@ -21,29 +21,15 @@ public class BallMovement : MonoBehaviour
 
     private void Start()
     {
-        // change default ball position :
-        transform.position = levelManager.defaultBallRoadTile.position;
+        transform.position = _levelManager.defaultBallRoadTile.position;
 
-        swipeListener.OnSwipe.AddListener(swipe =>
-        {
-            switch (swipe)
-            {
-                case "Right":
-                    moveDirection = Vector3.right;
-                    break;
-                case "Left":
-                    moveDirection = Vector3.left;
-                    break;
-                case "Up":
-                    moveDirection = Vector3.forward;
-                    break;
-                case "Down":
-                    moveDirection = Vector3.back;
-                    break;
-            }
+        _inputService.onChangedDirection += OnChangedDirection;
+    }
 
-            MoveBall();
-        });
+    private void OnChangedDirection(Vector2 direction)
+    {
+        moveDirection = new Vector3(direction.x, 0, direction.y);
+        MoveBall();
     }
 
     private void MoveBall()
@@ -52,7 +38,7 @@ public class BallMovement : MonoBehaviour
         {
             canMove = false;
             // add raycast in the swipe direction (from the ball) :
-            RaycastHit[] hits = Physics.RaycastAll(transform.position, moveDirection, MAX_RAY_DISTANCE, wallsAndRoadsLayer.value)
+            RaycastHit[] hits = Physics.RaycastAll(transform.position, moveDirection, MAX_RAY_DISTANCE, _wallsAndRoadsLayer.value)
                                        .OrderBy(hit => hit.distance).ToArray(); // added this line to sort tiles by distance from the ray's origin
 
             Vector3 targetPosition = transform.position;
@@ -63,31 +49,32 @@ public class BallMovement : MonoBehaviour
 
             for (int i = 0; i < hits.Length; i++)
             {
-               if (hits[i].collider.isTrigger)
-               {
-                  pathRoadTiles.Add(hits[i].transform.GetComponent<RoadTile>());
-               } else
-               {
-                  if (i == 0)
-                  {
-                     canMove = true;
-                     return;
-                  }
+                if (hits[i].collider.isTrigger)
+                {
+                    pathRoadTiles.Add(hits[i].transform.GetComponent<RoadTile>());
+                }
+                else
+                {
+                    if (i == 0)
+                    {
+                        canMove = true;
+                        return;
+                    }
 
-                  steps = i;
-                  targetPosition = hits[i - 1].transform.position;
-                  break;
-               }
+                    steps = i;
+                    targetPosition = hits[i - 1].transform.position;
+                    break;
+                }
             }
 
-            float moveDuration = stepDuration * steps;
+            float moveDuration = _stepDuration * steps;
             transform
                .DOMove(targetPosition, moveDuration)
                .SetEase(Ease.OutExpo)
                .OnComplete(() => canMove = true);
 
             if (onMoveStart != null)
-               onMoveStart.Invoke(pathRoadTiles, moveDuration);
+                onMoveStart.Invoke(pathRoadTiles, moveDuration);
         }
     }
 }
